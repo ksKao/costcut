a2781d9b2034d1c0913dd8f87dc5c3a650021089
Kai Siang Kao <kaokaisiang021001@gmail.com>
2023-07-29 19:12:54 +1000

    Added verify email in settings page

--- src/pages/Settings.svelte
+++ src/pages/Settings.svelte
@@ -4,8 +4,10 @@
 	import {
 		EmailAuthProvider,
 		reauthenticateWithCredential,
+		sendEmailVerification,
 		updatePassword,
 		updateProfile,
+		type User,
 	} from 'firebase/auth';
 	import { user } from '../stores/auth';
 	import { auth } from '../lib/firebase';
@@ -15,6 +17,7 @@
 	let username = $user?.displayName ?? $user?.email?.split('@')[0] ?? '';
 	let changePasswordLoading = false;
 	let changeUsernameLoading = false;
+	let verifyEmailLoading = false;
 
 	const changePassword = async () => {
 		if (!newPassword || !$user) return;
@@ -66,55 +69,88 @@
 			changeUsernameLoading = false;
 		}
 	};
+
+	const sendVerificationEmail = async () => {
+		if ($user === null || $user === undefined) return;
+		try {
+			verifyEmailLoading = true;
+			await sendEmailVerification($user);
+			toast.success(
+				'Verification email has been sent. Check your inbox and follow the instructions provided.'
+			);
+		} catch (e) {
+			toast.error('Something went wrong. Please try again later.');
+		} finally {
+			verifyEmailLoading = false;
+		}
+	};
 </script>
 
-<h1 class="mb-4 text-xl font-bold lg:text-2xl">Settings</h1>
-
-<h2 class="mb-2 text-lg font-semibold lg:text-xl">Change Password</h2>
-<form on:submit|preventDefault={changePassword}>
-	<div class="form-control mb-2">
-		<label class="label" for="currentPassword">
-			<span class="label-text">Current Password</span>
-		</label>
-		<input
-			placeholder="●●●●●●●●"
-			id="currentPassword"
-			type="password"
-			class="input-bordered input max-w-[300px]"
-			bind:value={currentPassword}
-		/>
+{#if $user === undefined}
+	<div class="flex h-full w-full items-center justify-center">
+		<span class="loading loading-spinner" />
 	</div>
-	<div class="form-control">
-		<label class="label" for="newPassword">
-			<span class="label-text">New Password</span>
-		</label>
-		<div class="flex gap-4">
+{:else if $user === null}
+	<div class="flex h-full w-full items-center justify-center">
+		<h1>Login to access your settings.</h1>
+	</div>
+{:else}
+	<h1 class="mb-4 text-xl font-bold lg:text-2xl">Settings</h1>
+
+	<h2 class="mb-2 text-lg font-semibold lg:text-xl">Change Password</h2>
+	<form on:submit|preventDefault={changePassword}>
+		<div class="form-control mb-2">
+			<label class="label" for="currentPassword">
+				<span class="label-text">Current Password</span>
+			</label>
 			<input
 				placeholder="●●●●●●●●"
-				id="newPassword"
+				id="currentPassword"
 				type="password"
-				class="input-bordered input w-[300px] max-w-[300px]"
-				bind:value={newPassword}
+				class="input-bordered input max-w-[300px]"
+				bind:value={currentPassword}
 			/>
-			<Button isLoading={changePasswordLoading}>Change</Button>
 		</div>
-	</div>
-</form>
-<h2 class="mb-2 mt-6 text-lg font-semibold lg:text-xl">Change Username</h2>
-<form on:submit|preventDefault={changeUsername}>
-	<div class="form-control">
-		<label class="label" for="username">
-			<span class="label-text">Username</span>
-		</label>
-		<div class="flex gap-4">
-			<input
-				placeholder="Username"
-				id="username"
-				type="text"
-				class="input-bordered input w-[300px] max-w-[300px]"
-				bind:value={username}
-			/>
-			<Button isLoading={changeUsernameLoading}>Change</Button>
+		<div class="form-control">
+			<label class="label" for="newPassword">
+				<span class="label-text">New Password</span>
+			</label>
+			<div class="flex gap-4">
+				<input
+					placeholder="●●●●●●●●"
+					id="newPassword"
+					type="password"
+					class="input-bordered input w-[300px] max-w-[300px]"
+					bind:value={newPassword}
+				/>
+				<Button isLoading={changePasswordLoading}>Change</Button>
+			</div>
 		</div>
-	</div>
-</form>
+	</form>
+	<h2 class="mb-2 mt-6 text-lg font-semibold lg:text-xl">Change Username</h2>
+	<form on:submit|preventDefault={changeUsername}>
+		<div class="form-control">
+			<label class="label" for="username">
+				<span class="label-text">Username</span>
+			</label>
+			<div class="flex gap-4">
+				<input
+					placeholder="Username"
+					id="username"
+					type="text"
+					class="input-bordered input w-[300px] max-w-[300px]"
+					bind:value={username}
+				/>
+				<Button isLoading={changeUsernameLoading}>Change</Button>
+			</div>
+		</div>
+	</form>
+	<h2 class="mb-2 mt-6 text-lg font-semibold lg:text-xl">Verify Email</h2>
+	{#if $user.emailVerified}
+		<p>Your email has been verified.</p>
+	{:else}
+		<Button on:click={sendVerificationEmail} isLoading={verifyEmailLoading}
+			>Send Verification Email</Button
+		>
+	{/if}
+{/if}
